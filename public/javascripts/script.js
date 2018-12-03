4ed26932cd77e35bb853e8490a9d396cd8f10c64
Rustam_Samigullin <rstsmg1@gmail.com>
2018-12-02 19:02:11 -0500

    design part is done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,3 +3,32 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+let otherNode = 'OAS BRIDGE';
+
+
+let logo = document.getElementById('logo');
+
+let a = document.getElementsByClassName('page')[0];
+
+console.log(a);
+console.log(a.firstElementChild.firstElementChild.innerHTML)
+
+
+if(window.innerWidth<=600){
+
+
+
+if(a.firstElementChild.firstElementChild.innerHTML!==otherNode)
+{
+  logo.style.display = "none"
+  console.log('not a home page')
+}
+}
+
+
+
+
+
+
+
