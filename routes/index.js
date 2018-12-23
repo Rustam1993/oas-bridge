72b0afafe8f2298ec26c4f87106b73ee855644a7
Rustam_Samigullin <rstsmg1@gmail.com>
2018-12-23 15:12:34 -0500

    added nodemailer and published via heroku service

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const router  = express.Router();
+const express    = require('express');
+const router     = express.Router();
+const nodemailer = require('nodemailer');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -23,6 +24,29 @@ router.get('/services', (req, res, next) => {
 router.get('/contact', (req, res, next) => {
   res.render('contact');
 });
+
+router.post('/send-email', (req, res, next) => {
+  let { name, phoneNumber, email, comments } = req.body;
+  let transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.emailAddress,
+      pass: process.env.emailPassword
+    }
+  });
+  transporter.sendMail({
+    from: email,
+    to: 'rstsmg1@gmail.com', 
+    subject: `${name}, ${phoneNumber}`, 
+    text: comments,
+  })
+  .then(info => {
+    res.redirect('/contact')
+    console.log(info)
+  } )
+  .catch(error => console.log('Error',error));
+});
+
 
 module.exports = router;
 
