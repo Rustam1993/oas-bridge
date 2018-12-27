a3f335dc5a6485caf2830d5da1af4c7e52e8e6de
Rustam_Samigullin <rstsmg1@gmail.com>
2018-12-27 18:45:54 -0500

    added access token

--- routes/index.js
+++ routes/index.js
@@ -36,7 +36,8 @@ router.post('/send-email', (req, res, next) => {
         user: process.env.emailAddress,
         clientId: '199652032781-aqu8uk1hgtgiu01r3j2ckj477vd5ug71.apps.googleusercontent.com',
         clientSecret: 'x0TW_nk6UuebtFBg5e3SJBm-',
-        refreshToken: '1/E3Q7y4TraN5BAqL1r1SdDO0knor9b0UDJTTn6rj0jIM'
+        refreshToken: '1/E3Q7y4TraN5BAqL1r1SdDO0knor9b0UDJTTn6rj0jIM',
+        accessToken: 'ya29.GluABsc1FLhp9GNH-xRwQOcfINSXYk6_xUY7Ik-tt56hxjtYauktY1a6d32NoHdT7YIQMdSTIq0dOfA8vf7aRfhIeDQXsbkur-hPH9BlzAkmDH7mznB-av4q_0_F',
     }
 });
   transporter.sendMail({
